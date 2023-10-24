8f26d630ea584d4876fc142a64938413243667a5
Blair Fleming <blair3003.bf@gmail.com>
2023-10-24 12:46:03 +0100

    Update Drawer.tsx

--- src/layouts/Drawer.tsx
+++ src/layouts/Drawer.tsx
@@ -3,15 +3,17 @@ import { useLayoutContext } from '../context/LayoutContext'
 const Drawer = () => {
 
     console.log(`Drawer rendered`)
+
+    const drawerStyle = `absolute right-0 z-50 translate-x-0 transition-transform data-[visible=false]:translate-x-full`
+    const closeButtonStyle = `w-full p-4 text-right`
     
     const { drawer, closeDrawer } = useLayoutContext()
 
     return (
-        {drawer &&
-        <aside>
-            <button type="button" onClick={() => closeDrawer()}>Close</button>
+        <aside data-visible={!!drawer} className={drawerStyle}>
+            <button type="button" onClick={() => closeDrawer()} className={closeButtonStyle}>Close</button>
             {drawer}
-        </aside>}
+        </aside>
     )
 }
 
