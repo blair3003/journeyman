dff18d646b4042ecda694f3a84b0e8185775abab
Blair Fleming <blair3003.bf@gmail.com>
2023-10-24 18:20:02 +0100

    tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import App from './App.tsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
