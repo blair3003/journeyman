f8c18740f1bfde34fafbc768d502b82f2be3ee35
Blair Fleming <blair3003.bf@gmail.com>
2023-10-27 12:04:14 +0100

    Update LayoutContext.tsx

--- src/context/LayoutContext.tsx
+++ src/context/LayoutContext.tsx
@@ -1,29 +1,31 @@
 import { createContext, useContext } from 'react'
 import useDrawer from '../hooks/useDrawer'
 
-const LayoutContext = createContext({})
-
 interface LayoutContextType {
     drawer: JSX.Element | null
     openDrawer: (content: JSX.Element) => void
     closeDrawer: () => null
 }
 
+const LayoutContext = createContext({})
+
 export const useLayoutContext = () => useContext(LayoutContext) as LayoutContextType
 
-const LayoutProvider = ({ children }: { children: React.ReactNode }) => {
+export const LayoutProvider = ({ children }: { children: React.ReactNode }) => {
 
     const { drawer, openDrawer, closeDrawer } = useDrawer()
 
+    const value: LayoutContextType = {
+        drawer,
+        openDrawer,
+        closeDrawer
+    }
+
     return (
-        <LayoutContext.Provider value={{
-            drawer,
-            openDrawer,
-            closeDrawer
-        }}>
+        <LayoutContext.Provider value={value}>
             {children}
         </LayoutContext.Provider> 
     )
 }
 
-export default LayoutProvider
+export default LayoutContext
