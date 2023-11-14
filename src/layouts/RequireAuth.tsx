e18ee4cda1eecfb2767e9a6bb753d71c4623c10f
Blair Fleming <blair3003.bf@gmail.com>
2023-11-14 12:42:17 +0000

    Update RequireAuth.tsx

--- src/layouts/RequireAuth.tsx
+++ src/layouts/RequireAuth.tsx
@@ -1,26 +1,15 @@
 import { useEffect, useState } from 'react'
 import { Navigate, Outlet } from 'react-router-dom'
-import { onAuthStateChanged } from 'firebase/auth'
+import { useAuthContext } from '../context/AuthContext'
 
 const RequireAuth = () => {
 
-	const [authenticated, setAuthenticated] = useState(false)
-	const [pending, setPending] = useState(true)
-
-	useEffect(() => {
-		const unsubscribe = onAuthStateChanged(auth, user => {
-			user
-			? setAuthenticated(true)
-			: setAuthenticated(false)
-			setPending(false)
-		})
-		return () => unsubscribe()
-	}, [])
-
+	const { auth, pending } = useAuthContext()
+	
 	if (pending) return null
 
 	return (
-        	authenticated
+        	auth
 		? <Outlet />
         	: <Navigate to="/login" replace />
 	)
