ec3f9c877f93ab37c5dac0d50f9e9f59856973ec
Blair Fleming <blair3003.bf@gmail.com>
2023-11-14 12:20:05 +0000

    Update useAuth.tsx

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,11 +1,10 @@
 import { useEffect, useState } from 'react'
 import { auth } from '../config/firebase'
 import { onAuthStateChanged } from 'firebase/auth'
-import usePersist from './usePersist'
 
 const useAuth = () => {
 
-    const { authUser, setAuthUser } = usePersist()
+    const { authUser, setAuthUser } = useState<FirebaseUser | null>(null)
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, user => setAuthUser(user))
