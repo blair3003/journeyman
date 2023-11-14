def42d2a2ddd8b7829884508fff035b7a5a4e9de
Blair Fleming <blair3003.bf@gmail.com>
2023-11-14 12:42:23 +0000

    Update useAuth.tsx

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -5,13 +5,17 @@ import { onAuthStateChanged } from 'firebase/auth'
 const useAuth = () => {
 
     const [authUser, setAuthUser] = useState<FirebaseUser | null>(null)
+    const [pending, setPending] = useState(true)
 
     useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, user => setAuthUser(user))
+        const unsubscribe = onAuthStateChanged(auth, user => {
+            setAuthUser(user)
+            setPending(false)
+        })
         return () => unsubscribe()
     }, [])
 
-    return authUser
+    return { authUser, pending }
 }
 
 export default useAuth
