d3da4ec2cd8cebb1b6a7f3286289115397fa7e55
Blair Fleming <blair3003.bf@gmail.com>
2023-11-17 13:08:40 +0000

    Create Label.tsx

---
+++ src/pages/campaign/components/Label.tsx
@@ -0,0 +1,26 @@
+import useMenu from '../../../hooks/useMenu'
+import Menu from '../../../components/Menu'
+
+interface LabelProps {
+	label: string
+	color: string
+	onRemove: (label: string) => void
+}
+
+const Label = ({ label, color, onRemove }: LabelProps) => {
+
+	const { menu, openMenu, closeMenu } = useMenu({
+		'Remove': () => onRemove(label)
+	})
+
+	return (
+		<div className="relative">
+			<button onClick={e => {e.preventDefault(); openMenu()}} className={`bg-${color} w-6 h-4`}>
+				<span key={label} className="sr-only">{label}</span>
+			</button>
+			<Menu menu={menu} onClose={closeMenu} />
+		</div>
+	)
+}
+
+export default Label
