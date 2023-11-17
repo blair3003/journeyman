1b182dd485151dd9560ca84477df968490f24c5b
Blair Fleming <blair3003.bf@gmail.com>
2023-11-17 13:06:37 +0000

    Create Labels.tsx

---
+++ src/pages/campaign/components/Labels.tsx
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+import { FieldErrors } from 'react-hook-form'
+import { HiPlus } from 'react-icons/hi2'
+import { options } from '../../../config/options'
+import useMenu from '../../../hooks/useMenu'
+import LabelsMenu from './LabelsMenu'
+import Label from './Label'
+
+interface LabelsProps {
+	defaultValues?: string[]
+	setValue: (name: string, value: unknown, config?: Object) => void
+	errors: FieldErrors
+}
+
+const Labels = ({ defaultValues = [], setValue, errors }: LabelsProps) => {
+
+	const { labels: labelOptions } = options
+	const [labels, setLabels] = useState(defaultValues)
+
+	const addLabel = (newLabel: string) => {
+		if (!labels.includes(newLabel)) {
+			setLabels(oldLabels => [...oldLabels, newLabel])
+			setValue('labels', labels)
+		}
+	}
+
+	const removeLabel = (oldLabel: string) => {
+		setLabels(oldLabels => oldLabels.filter(label => label !== oldLabel))
+		setValue('labels', labels)
+	}
+
+	const { menu, openMenu, closeMenu } = useMenu(
+		labelOptions.reduce((menu, key) => {
+			menu[key] = () => addLabel(key)
+			return menu
+		})
+	)
+
+
+	return (
+		<div className="bg-white border-gray-300 border-2 rounded p-2 mb-2">
+			<div className="flex justify-between items-center mb-1">
+				<span className="text-black uppercase font-bold text-xs">Labels</span>
+				{errors.labels && <span className="text-red-500 uppercase font-bold text-xs">{errors.labels?.message?.toString()}</span>}
+			</div>
+			<div className="flex justify-between items-center">
+				<div className="flex justify-start items-center">
+					{labels.map(label => <Label label={label} color={labelOptions[label]} onRemove={removeLabel} />)}
+				</div>
+				<div className="relative">
+					<button onClick={openMenu} className="flex items-center">
+						<span className="sr-only">Add Label</span>
+                        <HiPlus />
+					</button>
+					<LabelsMenu menu={menu} onClose={closeMenu} />
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Labels
