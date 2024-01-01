c75ff23cdae494645c787bcf4451f01e8b9d4f36
Blair Fleming <blair3003.bf@gmail.com>
2024-01-01 13:43:36 +0000

    Update DarkModeToggle.tsx

--- src/components/DarkModeToggle.tsx
+++ src/components/DarkModeToggle.tsx
@@ -8,6 +8,8 @@ const DarkModeToggle = () => {
     return (
         <button
             onClick={toggleDarkMode}
+            type="button"
+            title="Toggle Dark Mode"
             className={`p-2 ${isDarkMode ? "text-slate-100" : "text-slate-800"}`}
         >
             <span className="sr-only">{isDarkMode ? "Turn off Dark Mode" : "Turn on Dark Mode"}</span>
